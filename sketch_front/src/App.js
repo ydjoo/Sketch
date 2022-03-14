ea46221803e326dffb9979ad66dda0720fceef91
fegler <ydjoo12@gmail.com>
2022-03-14 23:56:59 +0900

    style transfer - image loading front page Layout setting

--- sketch_front/src/App.js
+++ sketch_front/src/App.js
@@ -1,32 +1,71 @@
-import { useState, Component } from "react";
-import InputList from "./InputList";
+import React from 'react';
+import styled from 'styled-components';
+import InputLoadUI from './UI/InputLoadUI';
 
-function App() {
-  const [inputImages, setInputImages] = useState([
-    {
-      id: 1,
-      imgName: "img1",
-      imgUrl: "/home/ydjoo/project/Sketch/sketch_back/images/inputs/input1.png",
-    },
-  ]);
-  const [outputImages, setOutputImages] = useState([
-    {
-      id: 1,
-      imgName: "img1",
-      imgUrl:
-        "/home/ydjoo/project/Sketch/sketch_back/images/outputs/output1.png",
-    },
-  ]);
+const WholePage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #FBF8F1;
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+`;
+const MainBox = styled.div`
+  width: 40vw;
+  height: 40vw;
+  background-color: #E9DAC1;
+  display: flex; 
+  flex-direction: column;
+`;
+const TitleDiv = styled.div`
+  width: 100%;
+  height: 20%;
+  background-color: #BF9270;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+`;
+const LayoutDiv = styled.div`
+  width: 100%;
+  height: ${props => props.height};
+  background-color: ${props => props.bgColor};
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+`;
+const ContentDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex; 
+  align-items: center; 
+  justify-content: space-between;
+`;
+
+const App = () => {
   return (
-    <h1>This is react result!!</h1> 
+    <WholePage>
+      <MainBox>
+        <LayoutDiv
+          height="20%"
+          bgColor="#BF9270"
+          style={{ color: "white", fontSize: "2rem" }}>
+          Style Transfer
+        </LayoutDiv>
+        <LayoutDiv
+          height="10%"
+        >
+          <InputLoadUI />
+        </LayoutDiv>
+        <ContentDiv>
+          <div style={{ border: "1px solid #54BAB9", width: "100%", height: "50%" }}></div>
+          <div style={{ width: "100%", height: "30%" }}></div>
+          <div style={{ border: "1px solid #54BAB9", width: "100%", height: "50%" }}></div>
+        </ContentDiv>
+      </MainBox>
+    </WholePage>
   );
-
-  // return (
-  //   <div>
-  //     <h3>Input Image List!</h3>
-  //     <InputList inputImages={inputImages} outputImages={outputImages} />
-  //   </div>
-  // );
 }
 
 export default App;
---
+++ sketch_front/src/UI/InputLoadUI.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputLoadUI = () => {
+  return (
+    <input type="file"></input>
+  );
+}
+
+export default InputLoadUI;
