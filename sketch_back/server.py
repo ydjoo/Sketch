7c1c00508eedd1e19cc1cf959b797a72edbabda7
fegler <ydjoo12@gmail.com>
2022-03-14 20:57:42 +0900

    Flask local server implementation & create-react-app build(hello world page)

--- sketch_back/server.py
+++ sketch_back/server.py
@@ -1,12 +1,15 @@
+import os 
 from flask import Flask, render_template, send_from_directory
 
-# app = Flask(__name__, static_folder='C:/Users/82107/Desktop/DJ/Sketch/sketch_front/build')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../sketch_front/build')
 
-@app.route("/")
-def hello():
-    return '<h1>Hello World!!</h1>'
-    # return send_from_directory(app.static_folder, 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', port=8888, debug=True)
