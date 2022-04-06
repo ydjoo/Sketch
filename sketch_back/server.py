912ce88834d7079d0fefda6a04d9a984c46169f9
ydjoo <djyun@hanyang.ac.kr>
2022-04-06 09:39:40 +0900

    image file load & preview(only using front) to input button tag division

--- sketch_back/server.py
+++ sketch_back/server.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, static_folder='../sketch_front/build')
 
-@app.route('/', defaults={'path': ''})
+@app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8888, debug=True)
--- sketch_front/src/UI/InputLoadUI.js
+++ sketch_front/src/UI/InputLoadUI.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const InputLoadUI = () => {
+  const [selectedFile, setSelectedFile] = useState();
+  const [previewFile, setPreviewFile] = useState();
+
+  // select event 발생 시 preview file state 변경 effect 
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreviewFile(undefined)
+      return;
+    }
+    const objectURL = URL.createObjectURL(selectedFile);
+    setPreviewFile(objectURL);
+  }, [selectedFile])
+
+  // 버튼 클릭을 통해 upload 파일 선택 시 invoke 
+  const onSelect = e => {
+    setSelectedFile(e.target.files[0]);
+  }
+
   return (
-    <input type="file"></input>
+    <>
+      <input type="file" onChange={onSelect}></input>
+      {previewFile && <img src={previewFile} />}
+    </>
   );
 }
 
