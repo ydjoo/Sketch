7a0777116d60ff12adbe452809851cb3e28f4c97
richardSHkim <kshtrue95@gmail.com>
2022-04-14 10:17:55 +0900

    space

--- client/src/components/PostImage.js
+++ client/src/components/PostImage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 
+
 function PostImage(props) {
     const [userInput, setUserInput] = useState()
 
--- client/src/components/ShowImage.js
+++ client/src/components/ShowImage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function ShowImage(props) {
     return (
         <>
